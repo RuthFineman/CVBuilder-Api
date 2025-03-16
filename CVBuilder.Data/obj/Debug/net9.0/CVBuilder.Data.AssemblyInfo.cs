466737bb29f94a0adaa1e4f7ff578586d70b7c25
RuthFineman <ruti8588@gmail.com>
2025-03-16 17:58:00 +0200

    update key

--- CVBuilder.Data/obj/Debug/net9.0/CVBuilder.Data.AssemblyInfo.cs
+++ CVBuilder.Data/obj/Debug/net9.0/CVBuilder.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CVBuilder.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c91a0ebfbb7ba306c840383b87cbabe58be9f2f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CVBuilder.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CVBuilder.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CVBuilder.Service/obj/Debug/net9.0/CVBuilder.Service.AssemblyInfo.cs
+++ CVBuilder.Service/obj/Debug/net9.0/CVBuilder.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CVBuilder.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c91a0ebfbb7ba306c840383b87cbabe58be9f2f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CVBuilder.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CVBuilder.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
