8de055d6f5efc4d011629f4280e6e2e5ba373af5
RuthFineman <ruti8588@gmail.com>
2025-05-30 15:18:08 +0300

    create DockerFile

--- CVBuilder.Api/obj/Debug/net9.0/CVBuilder.Api.AssemblyInfo.cs
+++ CVBuilder.Api/obj/Debug/net9.0/CVBuilder.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CVBuilder.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c9c7e4470946c2c121f0fd907c1fe3a7738b852")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ec1aa15339025269d66bf546e29fe6f21d9454d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CVBuilder.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CVBuilder.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
